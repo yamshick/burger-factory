9f4218f030aeefeed4adda4ce33155918aab7e2a
danila <danila.it@rambler.ru>
2023-01-20 12:25:26 +0300

    history api fallback in dev mod

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,10 +6,12 @@ module.exports = merge(common, {
   mode: "development",
   output: {
     filename: "[name].js",
+    publicPath: "/",
   },
   devServer: {
     port: 3000,
     static: path.join(__dirname, "src"),
+    historyApiFallback: true,
   },
   devtool: "inline-source-map",
 });
